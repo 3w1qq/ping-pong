26078c7b086c06d788c7c1bf15f3a30eccb8e732
3w1qq <varyashabalova@mail.ru>
2025-09-02 21:51:35 +0400

    Add game scene

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 import pygame as p
 
+LIGHTPINK = (255, 182, 193)
+
+win_width, win_height = 700, 500
+window = p.display.set_mode((win_width, win_height))
+window.fill(LIGHTPINK)
+p.display.set_caption('Пинг понг')
+timer = p.time.Clock()
+
+run = True 
+
+while run:
+    events = p.event.get()
+    for event in events:
+        if event.type == p.QUIT:
+            run = False
+
+    p.display.update()
+    timer.tick(60)
